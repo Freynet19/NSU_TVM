097ec83a59453744609f09ba21deebde80c55ea9
Freynet19 <smirnov-gordey@mail.ru>
2025-10-13 10:20:17 +0700

    add lab03 lab04

--- lab03/src/calculate.ts
+++ lab03/src/calculate.ts
@@ -5,6 +5,71 @@ export const arithSemantics: ArithSemantics = grammar.createSemantics() as Arith
 
 
 const arithCalc = {
+    Expr: function (expr) {
+        return expr.calculate(this.args.params);
+    },
+
+    AddExp(first, operator, rest) {
+        let res = first.calculate(this.args.params);
+        const len = operator.children.length;
+        for (let i = 0; i < len; i++) {
+            const op = operator.child(i).sourceString;
+            const rVal = rest.child(i).calculate(this.args.params);
+            switch (op) {
+                case '+':
+                    res += rVal;
+                    break;
+                case '-':
+                    res -= rVal;
+                    break;
+            }
+        }
+        return res;
+    },
+
+    MulExp(first, operator, rest) {
+        let res = first.calculate(this.args.params);
+        const len = operator.children.length;
+        for (let i = 0; i < len; i++) {
+            const op = operator.child(i).sourceString;
+            const rVal = rest.child(i).calculate(this.args.params);
+            switch (op) {
+                case '*':
+                    res *= rVal;
+                    break;
+                case '/':
+                    if (rVal === 0) throw new Error("division by zero");
+                    res /= rVal;
+                    break;
+                default: throw new Error();
+            }
+        }
+        return res;
+    },
+
+    AtomExp_number: function (num) {
+        return num.calculate(this.args.params);
+    },
+    AtomExp_un_minus: function (_minus, expr) {
+        return -expr.calculate(this.args.params);
+    },
+    AtomExp_variable(_name) {
+        const varName = this.sourceString;
+        if (this.args.params && this.args.params[varName] !== undefined) {
+            return this.args.params[varName];
+        }
+        return NaN;
+    },
+    AtomExp_paren(_open_paren, expr, _closeParen) {
+        return expr.calculate(this.args.params);
+    },
+
+    number_float(_intPart, _dot, _fracPart) {
+        return parseFloat(this.sourceString);
+    },
+    number_int(_int) {
+        return parseInt(this.sourceString, 10);
+    },
 } satisfies ArithmeticActionDict<number | undefined>;
 
 
--- lab04/src/ast.ts
+++ lab04/src/ast.ts
@@ -1,1 +1,36 @@
-export type Expr = {};
+export type Expr = Number | Variable | Negative | Binary;
+
+export interface Number {
+    type: 'num';
+    value: number;
+}
+
+export interface Variable {
+    type: 'var';
+    name: string;
+}
+
+export interface Negative {
+    type: 'neg';
+    arg: Expr;
+}
+
+export interface Binary {
+    type: 'bin';
+    left: Expr;
+    operation: '+' | '-' | '*' | '/';
+    right: Expr;
+}
+
+export const bin = (left: Expr, right: Expr, operation: '+' | '-' | '*' | '/'): Expr => ({
+    type: 'bin',
+    operation,
+    left,
+    right
+});
+
+export const mul = (left: Expr, right: Expr): Expr => bin(left, right, '*');
+export const div = (left: Expr, right: Expr): Expr => bin(left, right, '/');
+export const sum = (left: Expr, right: Expr): Expr => bin(left, right, '+');
+export const sub = (left: Expr, right: Expr): Expr => bin(left, right, '-');
+
