e3fb93396b72d5f2928570dcf8a344b9397f74b4
Freynet19 <smirnov-gordey@mail.ru>
2025-10-26 19:20:13 +0700

    add lab05

--- lab05/src/compiler.ts
+++ lab05/src/compiler.ts
@@ -1,10 +1,29 @@
-import { c as C, Op, I32 } from "../../wasm";
-import { Expr } from "../../lab04";
-import { buildOneFunctionModule, Fn } from "./emitHelper";
-const { i32, get_local} = C;
-    
+import {c as C, I32, Op} from "../../wasm";
+import {Expr} from "../../lab04";
+import {buildOneFunctionModule, Fn} from "./emitHelper";
+
+const {i32, get_local} = C;
+
 export function getVariables(e: Expr): string[] {
-    throw "Not implemented";
+    const s: string[] = [];
+    (function walk(x: Expr) {
+        switch (x.type) {
+            case 'num':
+                break;
+            case 'var':
+                if (s.findIndex(e => e == x.name) < 0)
+                    s.push(x.name);
+                break;
+            case 'neg':
+                walk(x.arg);
+                break;
+            case 'bin':
+                walk(x.left);
+                walk(x.right);
+                break;
+        }
+    })(e);
+    return s;
 }
 
 export async function buildFunction(e: Expr, variables: string[]): Promise<Fn<number>>
@@ -13,6 +32,35 @@ export async function buildFunction(e: Expr, variables: string[]): Promise<Fn<nu
     return await buildOneFunctionModule("test", variables.length, [expr]);
 }
 
-function wasm(e: Expr, args: string[]): Op<I32> {
-    throw "Not implemented";
+function wasm(e: Expr, args: readonly string[]): Op<I32> {
+    const nameToIndex = new Map<string, number>();
+    for (let i = 0; i < args.length; i++) nameToIndex.set(args[i], i);
+
+    function emit(x: Expr): Op<I32> {
+        switch (x.type) {
+            case 'num':
+                return i32.const(x.value);
+            case 'var': {
+                const idx = nameToIndex.get(x.name);
+                if (idx == null) throw new Error(`Unknown variable: ${x.name}`);
+                return get_local(i32, idx);
+            }
+            case 'neg':
+                return i32.sub(i32.const(0), emit(x.arg));
+            case 'bin':
+                switch (x.operation) {
+                    case '+':
+                        return i32.add(emit(x.left), emit(x.right));
+                    case '-':
+                        return i32.sub(emit(x.left), emit(x.right));
+                    case '*':
+                        return i32.mul(emit(x.left), emit(x.right));
+                    case '/':
+                        return i32.div_s(emit(x.left), emit(x.right));
+                }
+        }
+    }
+
+    return emit(e);
 }
+
